855ffa0011a0419bca7cd5152bfc20b7c377794d
DESKTOP-C9PF2AS\user <eeww95@gmail.com>
2017-12-19 18:09:01 +0900

    add xml(naming) make login activity

---
+++ Library/app/src/main/java/com/example/user/library/activity/MainActivity.java
@@ -0,0 +1,18 @@
+package com.example.user.library.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.example.user.library.R;
+
+/**
+ * Created by user on 2017-12-19.
+ */
+
+public class MainActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
