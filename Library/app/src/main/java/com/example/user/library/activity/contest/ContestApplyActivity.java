dacfb250b9605e40b3fa828ac1acfd8ae86d7346
DESKTOP-C9PF2AS\user <eeww95@gmail.com>
2017-12-20 05:57:52 +0900

    change contest

--- Library/app/src/main/java/com/example/user/library/activity/contest/ContestApplyActivity.java
+++ Library/app/src/main/java/com/example/user/library/activity/contest/ContestApplyActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.ImageButton;
+import android.widget.TextView;
 
 import com.example.user.library.R;
 
@@ -21,6 +22,10 @@ public class ContestApplyActivity extends AppCompatActivity {
     private ImageButton bBook;
     private CheckBox agreeCheck;
     private Button submit;
+    private TextView selected;
+    private String a = "수학하는 신체";
+    private String b ="국가란 무엇인가";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -29,8 +34,26 @@ public class ContestApplyActivity extends AppCompatActivity {
         int n = 4;
         setTitle("제 "+n+"회 독서 경시대회");
 
+        selected = (TextView)findViewById(R.id.contest_apply_slected);
+
         aBook = (ImageButton)findViewById(R.id.contest_apply_abook);
+        aBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selected.setText(a);
+            }
+        });
+
+
         bBook = (ImageButton)findViewById(R.id.contest_apply_bbook);
+        bBook.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                selected.setText(b);
+            }
+        });
+
+
         agreeCheck = (CheckBox)findViewById(R.id.contest_apply_check);
         submit = (Button)findViewById(R.id.contest_apply_submit);
 
@@ -40,6 +63,9 @@ public class ContestApplyActivity extends AppCompatActivity {
 
                 if(!agreeCheck.isChecked())
                     return;
+                else if( selected.getText().equals("")){
+                    return;
+                }
                 else {
                     LayoutInflater inflater = (LayoutInflater) getApplicationContext().getSystemService(LAYOUT_INFLATER_SERVICE);
                     View mView = inflater.inflate(R.layout.contest_apply_dialog, null);
